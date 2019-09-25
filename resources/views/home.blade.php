329bd85c58e702c5e3f7743218a3ffcf413b6464
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-09-25 20:24:06 -0300

    Ajustes no redirecionamento caso logado (ao invés de home, dashboard)
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
+@section('title', 'Página Principal')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <section id="user-selection">
 
-                    You are logged in!
-                </div>
+        <div class="row">
+            <div class="col-7">
+                <h2 class="mt-2rem">A plataforma perfeita para as suas necessidades. Seja você um paciente, ou médico.</h2>
+                <p>Com a nossa plataforma, marcar uma consulta vai ser mais fácil do que nunca.</p>
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="choices">
+            <a href="#!" class="choice">
+                <div class="circle patients"></div>
+                <p>Sou um paciente</p>
+            </a>
+            <a href="#!" class="choice">
+                <div class="circle doctors"></div>
+                <p>Sou um médico</p>
+            </a>
+        </div>
+
+    </section>
+    
 @endsection
