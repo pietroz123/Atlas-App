e04e3a878e07a4235f9d47936282f956f417d9ed
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-09-25 16:04:31 -0300

    Mais CSS
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,19 @@
 
 @section('title', 'Página Principal')
 
+<style>
+    body {
+        background: linear-gradient(90deg, white, #ccebff);
+    }
+</style>
+
 @section('content')
 
     <section id="user-selection">
 
         <div class="row">
             <div class="col-7">
-                <h2 class="mt-5rem">A plataforma perfeita para as suas necessidades. Seja você um paciente, ou médico.</h2>
+                <h2 class="mt-2rem">A plataforma perfeita para as suas necessidades. Seja você um paciente, ou médico.</h2>
                 <p>Com a nossa plataforma, marcar uma consulta vai ser mais fácil do que nunca.</p>
             </div>
         </div>
