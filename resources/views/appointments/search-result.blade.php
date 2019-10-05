106c156490232d2558c03d1ee2df538332ee28f7
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-10-05 18:26:35 -0300

    Teste com mapa
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- resources/views/appointments/search-result.blade.php
+++ resources/views/appointments/search-result.blade.php
@@ -98,7 +98,7 @@
                     </div>
                 </div>
                 <div class="col">
-
+                    <img border="0" src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key={{ env('GOOGLE_MAPS_API') }}" alt="Points of Interest in Lower Manhattan">
                 </div>
             </div>
         </div>
