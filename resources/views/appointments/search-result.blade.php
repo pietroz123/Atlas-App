6d65f50a35dfd8595bc36f9a23a8bd54a962e0b3
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-10-05 17:18:33 -0300

    Nova barra de navegação da página de busca
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- resources/views/appointments/search-result.blade.php
+++ resources/views/appointments/search-result.blade.php
@@ -4,18 +4,65 @@
 
 @section('header-bar')
     <header id="header-search">
-        <div class="header-logo">
-            <img src="{{ asset('img/icons/atlas-heart.png') }}" alt="Logo do Atlas" style="width: 49px">
-        </div>
-        <div class="input-wrapper">
-            <div class="input-icon">
-                <i class="fas fa-search"></i>
+        <nav class="navbar navbar-expand-md navbar-light w-100" id="search-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('img/icons/atlas-heart.svg') }}" class="main-icon" alt="Ícone principal da página">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+        
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        <li>
+                            <!-- Search Input Box -->
+                            <div class="input-wrapper">
+                                <div class="input-icon">
+                                    <i class="fas fa-search"></i>
+                                </div>
+                                <input type="text" value="Sorocaba - SP" class="input-search">
+                            </div>
+                        </li>
+                    </ul>
+        
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        @yield('right-links')
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+        
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+        
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-            <input type="text" value="Sorocaba - SP" class="input-search">
-        </div>
-        <div>
-            <a href=""></a>
-        </div>
+        </nav>
     </header>
 @endsection
 
