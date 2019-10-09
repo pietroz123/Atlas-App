b77741e4a59ffe461c896da4030f7c55a37d266a
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-10-09 00:23:56 -0300

    Criação das tabs do médico
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- resources/views/doctor-profile.blade.php
+++ resources/views/doctor-profile.blade.php
@@ -43,7 +43,51 @@
                     <i class="fas fa-star-half-alt"></i>
                     <span>(18 avaliações)</span>
                 </div>
-                
+
+                <ul class="nav nav-tabs mt-4" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link sobre active" id="sobre-tab" data-toggle="tab" href="#sobre" role="tab" aria-controls="sobre"
+                            aria-selected="true">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link agenda" id="agenda-tab" data-toggle="tab" href="#agenda" role="tab" aria-controls="agenda"
+                            aria-selected="false">Agenda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link avaliacoes" id="avaliacoes-tab" data-toggle="tab" href="#avaliacoes" role="tab" aria-controls="avaliacoes"
+                            aria-selected="false">Avaliações</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="sobre" role="tabpanel" aria-labelledby="sobre-tab">
+                        Raw denim you
+                        probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master
+                        cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro
+                        keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip
+                        placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi
+                        qui.
+                    </div>
+                    <div class="tab-pane fade" id="agenda" role="tabpanel" aria-labelledby="agenda-tab">
+                        Food truck fixie
+                        locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit,
+                        blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.
+                        Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum
+                        PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS
+                        salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit,
+                        sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
+                        stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.
+                    </div>
+                    <div class="tab-pane fade" id="avaliacoes" role="tabpanel" aria-labelledby="avaliacoes-tab">
+                        Etsy mixtape
+                        wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack
+                        lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard
+                        locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify
+                        squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie
+                        etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog
+                        stumptown. Pitchfork sustainable tofu synth chambray yr.
+                    </div>
+                </div>
+
             </section>
             {{-- END DOCTOR INFORMATION --}}
     
