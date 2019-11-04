53f68532f1ec2dc604edcca3fff236725739a4c5
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-11-04 18:40:13 -0300

    Criação do model e da migração de agendamentos
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

---
+++ database/migrations/2019_11_04_212434_create_appointments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('patient_id')->unsigned();
+            $table->bigInteger('doctor_id')->unsigned();
+            $table->time('probable_start_time');
+            $table->time('probable_end_time');
+            $table->enum('status', ['active', 'canceled', 'complete']);
+            $table->date('appointment_date');
+            $table->timestamps();
+        });
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
