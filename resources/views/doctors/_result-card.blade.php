4fa45746f2fcfa092e6517e249957640f3ce60e5
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-11-03 23:08:30 -0300

    Slots de horários

--- resources/views/doctors/_result-card.blade.php
+++ resources/views/doctors/_result-card.blade.php
@@ -51,18 +51,30 @@
                                     <span class="day-date">{{ date('d M', strtotime($date)) }}</span>
                                 </div>
                                 <div class="calendar-day-slots d-flex flex-column">
-                                    <a href="#!" class="calendar-slot available">13:00</a>
-                                    <a href="#!" class="calendar-slot available">13:15</a>
-                                    <a href="#!" class="calendar-slot available">13:30</a>
-                                    <a href="#!" class="calendar-slot not-available" title="Horário indisponível">13:45</a>
-                                    <a href="#!" class="calendar-slot available">14:00</a>
-                                    <a href="#!" class="calendar-slot not-available" title="Horário indisponível">14:15</a>
-                                    <a href="#!" class="calendar-slot not-available" title="Horário indisponível">14:30</a>
-                                    <a href="#!" class="calendar-slot not-available" title="Horário indisponível">14:45</a>
-                                    <a href="#!" class="calendar-slot available">15:00</a>
-                                    <a href="#!" class="calendar-slot available">15:15</a>
-                                    <a href="#!" class="calendar-slot available">15:30</a>
-                                    <a href="#!" class="calendar-slot available">15:45</a>
+                                    @php
+                                        $availabilities = $doctor->availabilities;
+                                        $morning = $availabilities->where('period', 'morning')->first();
+                                        $afternoon = $availabilities->where('period', 'afternoon')->first();
+                                        $morningStart = date('H:i', strtotime($morning->start_time));
+                                    @endphp
+                                    @while (strtotime($morningStart) < strtotime($morning->end_time))
+                                        <a href="#!" class="calendar-slot available">{{ $morningStart }}</a>
+                                        @php
+                                            $morningStart = date('H:i', strtotime("+15 minutes", strtotime($morningStart)) );
+                                        @endphp
+                                    @endwhile
+                                    @for ($i = 0; $i < 3; $i++)
+                                        <a href="#!">-</a>
+                                    @endfor
+                                    @php
+                                        $afternoonStart = date('H:i', strtotime($afternoon->start_time));
+                                    @endphp
+                                    @while (strtotime($afternoonStart) < strtotime($afternoon->end_time))
+                                        <a href="#!" class="calendar-slot available">{{ $afternoonStart }}</a>
+                                        @php
+                                            $afternoonStart = date('H:i', strtotime("+15 minutes", strtotime($afternoonStart)) );
+                                        @endphp
+                                    @endwhile
                                 </div>
                             </div>
                             @php
@@ -89,6 +101,6 @@
 
     </div>
     <div class="col-4">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.6077173877284!2d-47.46507688502323!3d-23.510635084708092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58a9305955f59%3A0xb878765355f9014d!2sAv.%20Bar%C3%A3o%20de%20Tatu%C3%AD%2C%20Sorocaba%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1570405930432!5m2!1spt-BR!2sbr" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.6077173877284!2d-47.46507688502323!3d-23.510635084708092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58a9305955f59%3A0xb878765355f9014d!2sAv.%20Bar%C3%A3o%20de%20Tatu%C3%AD%2C%20Sorocaba%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1570405930432!5m2!1spt-BR!2sbr" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe> --}}
     </div>
 </div>
